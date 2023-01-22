bee638fca616fe8440d9319ecadcbb3eec54cc93
Ryan Connaughton <hello@ryanconnaughton.com>
2023-01-22 11:47:10 +0000

    mv: css import body -> head

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import './index.css'
-
 function App() {
-
   return (
-      <div className="min-h-screen flex justify-center items-center">
-        <h1 className="text-3xl font-bold text-blue-600">
+    <div className="min-h-screen flex justify-center items-center">
+      <h1 className="text-3xl font-bold text-blue-600">
         Install & Setup Vite + React + Typescript + Tailwind CSS 3
-        </h1>
-      </div>
-  )
+      </h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
