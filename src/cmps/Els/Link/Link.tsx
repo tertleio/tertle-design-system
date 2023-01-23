6a3b078266c9cb4e761bdadaa64a845dbaef8a6c
Ryan Connaughton <hello@ryanconnaughton.com>
2023-01-23 19:45:16 +0000

    add: (hyper) Link component

---
+++ src/cmps/Els/Link/Link.tsx
@@ -0,0 +1,14 @@
+import { Link as RouterLink, LinkProps } from 'react-router-dom';
+
+const Link = ({ className = '', children, ...props }: LinkProps) => {
+  return (
+    <RouterLink
+      className={`text-blue-600 hover:text-blue-900, ${className}`}
+      {...props}
+    >
+      {children}
+    </RouterLink>
+  );
+};
+
+export { Link };
