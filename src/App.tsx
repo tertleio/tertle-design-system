a3860a97e8c6a07bc7e0302fa3e8db2f2fee5bf9
Ryan Connaughton <hello@ryanconnaughton.com>
2023-01-23 18:45:13 +0000

    btn: add additional variants & styles & test view

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,40 @@ import { Btn } from './cmps/Els/Btn';
 
 function App() {
   return (
-    <div className="min-h-screen flex justify-center items-center">
-      <h1 className="text-3xl font-bold text-green-600">Test View</h1>
-      <Btn className="custom-test-class">Big ol' button!</Btn>
+    <div className="min-h-screen justify-center items-center">
+      <div className="flex flex-column justify-center gap-3 m-10">
+        <h1 className="text-3xl font-bold text-green-600">Btn</h1>
+        <div>
+          <Btn variant="primary" size="sm">
+            primary sm
+          </Btn>
+        </div>
+        <div>
+          <Btn variant="primary" size="md">
+            primary md
+          </Btn>
+        </div>
+        <div>
+          <Btn variant="primary" size="lg">
+            primary lg
+          </Btn>
+        </div>
+        <div>
+          <Btn variant="secondary" size="sm">
+            secondary sm
+          </Btn>
+        </div>
+        <div>
+          <Btn variant="secondary" size="md">
+            secondary md
+          </Btn>
+        </div>
+        <div>
+          <Btn variant="secondary" size="lg">
+            secondary lg
+          </Btn>
+        </div>
+      </div>
     </div>
   );
 }
--- src/cmps/Els/Btn/Btn.tsx
+++ src/cmps/Els/Btn/Btn.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
 const variants = {
-  primary: 'bg-green-600 text-white',
+  primary: 'bg-green-600 text-white font-proxima-nova',
+  secondary: 'border border-1 border-white text-white',
   inverse: 'bg-white text-blue-600',
   danger: 'bg-red-600 text-white',
 };
@@ -35,7 +36,9 @@ const Btn = React.forwardRef<HTMLButtonElement, BtnProps>(
         ref={ref}
         type={type}
         className={`
-          flex justify-center items-center disabled:opacity-70 disabled:cursor-not-allowed rounded-lg font-medium focus:outline-none hover:opacity-90
+          flex justify-center items-center
+          font-primary font-medium uppercase rounded-lg
+          focus:outline-none hover:opacity-90 disabled:opacity-70 disabled:cursor-not-allowed
           ${variants[variant]}
           ${sizes[size]}
           ${className}`}
