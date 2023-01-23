45325edaf2e6178a42c478e5f8a548e1b0e5d8f0
Ryan Connaughton <hello@ryanconnaughton.com>
2023-01-23 19:46:09 +0000

    test: update with test Link & router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Btn } from './cmps/Els/Btn';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Btn, Link } from '@/cmps/Els';
 
 function App() {
   return (
@@ -35,6 +36,12 @@ function App() {
             secondary lg
           </Btn>
         </div>
+      </div>
+      <div className="flex flex-column justify-center gap-3 m-10">
+        <h1 className="text-3xl font-bold text-green-600">Links</h1>
+        <Router>
+          <Link to="https://tertle.io">Some link</Link>
+        </Router>
       </div>
     </div>
   );
