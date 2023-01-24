18827ec0a26ac3e77cd740c90698daedb27b4349
Ryan Connaughton <hello@ryanconnaughton.com>
2023-01-24 20:45:23 +0000

    add: initial List (checkbox | radio) component

---
+++ src/cmps/Form/List.tsx
@@ -0,0 +1,21 @@
+import { FieldWrapper, FieldWrapperPassProps } from './FieldWrapper';
+
+type ListProps = FieldWrapperPassProps & {
+  name: string;
+  type: 'radio' | 'checkbox';
+  value: string;
+};
+
+const List = (props: ListProps) => {
+  const { type, label, value, name, id } = props;
+
+  // if type = checkbox | radio -> map
+
+  return (
+    <FieldWrapper label={label} id={value}>
+      <input name={name} id={value} type={type} value={value} />
+    </FieldWrapper>
+  );
+};
+
+export { List };
