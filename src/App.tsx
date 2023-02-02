7f71bcea29604165260fd493eb393aa750bb4970
Ryan Connaughton <hello@ryanconnaughton.com>
2023-02-02 17:11:06 +0000

    cleanup: test view

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Btn, Link } from '@/cmps/Els';
 import { Form, Dropdown, List, Text, Textarea } from '@/cmps/Form';
+import { Container } from '@/cmps/Container';
 
 const opts = [
   {
@@ -26,8 +27,8 @@ function App() {
   }
 
   return (
-    <div className="min-h-screen justify-center items-center">
-      <div className="flex flex-column justify-center gap-3 m-1">
+    <>
+      <div className="flex justify-center gap-3 m-1">
         <h1 className="text-3xl font-bold text-green-600">Btn</h1>
         <div>
           <Btn variant="primary" size="sm">
@@ -66,28 +67,45 @@ function App() {
           <Link to="https://tertle.io">Some link</Link>
         </Router>
       </div>
-      <Form id="formId" onSubmit={handleSubmit}>
-        <div className="flex justify-items-center">
-          <Dropdown name="selectKey" label="Some label" opts={opts} />
-        </div>
-        <div className="flex justify-items-center">
-          <label className="text-green-500">Some section label</label>
-          <List type="radio" name="inputKey" value="1" label="Radio-1-label" />
-          <List type="radio" name="inputKey" value="2" label="Radio-2-label" />
-        </div>
-        <div className="flex justify-items-center text-red-500">
-          <Text name="textFieldKey" value={text} cb={setText} />
-          <Textarea
-            name="textareaFieldKey"
-            value={textarea}
-            rows={10}
-            cols={30}
-            cb={setTextarea}
-          />
-        </div>
-        <Btn type="submit">Submit</Btn>
-      </Form>
-    </div>
+
+      <div className="mx-auto mw-xl max-w-xl">
+        <Container>
+          <Form id="formId" onSubmit={handleSubmit}>
+            <div className="flex justify-items-center">
+              <Dropdown name="selectKey" label="Some label" opts={opts} />
+            </div>
+            <div className="flex-1 direction justify-items-center border border-red-500">
+              <label className="text-green-500 border">
+                Some section label
+              </label>
+              <List
+                type="radio"
+                name="inputKey"
+                value="1"
+                label="Radio-1-label"
+              />
+              <List
+                type="radio"
+                name="inputKey"
+                value="2"
+                label="Radio-2-label"
+              />
+            </div>
+            <div className="flex justify-items-center text-red-500">
+              <Text name="textFieldKey" value={text} cb={setText} />
+              <Textarea
+                name="textareaFieldKey"
+                value={textarea}
+                rows={10}
+                cols={30}
+                cb={setTextarea}
+              />
+            </div>
+            <Btn type="submit">Submit</Btn>
+          </Form>
+        </Container>
+      </div>
+    </>
   );
 }
 
