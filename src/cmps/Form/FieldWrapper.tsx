7f833eddcaa29d71fd6b50bfd05f87d479c9b244
Ryan Connaughton <hello@ryanconnaughton.com>
2023-02-02 19:35:10 +0000

    update: List / Wrapper styles & props

--- src/cmps/Form/FieldWrapper.tsx
+++ src/cmps/Form/FieldWrapper.tsx
@@ -4,22 +4,27 @@ export type FieldWrapperProps = {
   children: ReactNode;
   className?: string;
   label?: string;
-  description?: string;
   info?: string;
-  error?: string;
   id?: string;
+  error?: string;
 };
 
 type FieldWrapperPassProps = Omit<FieldWrapperProps, 'children' | 'className'>;
 
 const FieldWrapper = ({ children, error, ...props }: FieldWrapperProps) => {
-  const { label, id } = props;
+  const { label, id, className } = props;
+
   return (
     <>
-      <label htmlFor={id} className="text-white">
+      <label
+        htmlFor={id}
+        className={`flex cursor-pointer rounded-lg border border-gray-300 bg-white p-3
+        hover:bg-gray-50 focus:outline-none peer-checked:border-transparent peer-checked:ring-2 peer-checked:ring-green-500
+        ${className}`}
+      >
+        <div>{children}</div>
         {label}
       </label>
-      <div>{children}</div>
       {error && <div>{error}</div>}
     </>
   );
--- src/cmps/Form/List.tsx
+++ src/cmps/Form/List.tsx
@@ -4,14 +4,26 @@ type ListProps = FieldWrapperPassProps & {
   name: string;
   type: 'radio' | 'checkbox';
   value: string;
+  className?: string;
 };
+
+// tw examples: https://freefrontend.com/tailwind-radio-buttons/
+
+// 1. vertical or horizontal (easily changeable)
+// 2. Needs to wrap inside a box to selctable (mobile-like taps)
 
 const List = (props: ListProps) => {
   const { type, label, value, name, id } = props;
 
   return (
     <FieldWrapper label={label} id={value}>
-      <input name={name} id={value} type={type} value={value} />
+      <input
+        name={name}
+        id={value}
+        type={type}
+        value={value}
+        className="focus:ring-3 h-3 w-7 border-red-300 focus:ring-blue-300"
+      />
     </FieldWrapper>
   );
 };
