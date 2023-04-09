cd915102cd1e3b2430b7dc643484dabaf904d81a
Ryan Connaughton <hello@ryanconnaughton.com>
2023-04-09 18:18:33 +0100

    rm: redundant code from previous commit

--- src/cmps/Layouts/MainLayout.tsx
+++ src/cmps/Layouts/MainLayout.tsx
@@ -5,7 +5,7 @@ type MainLayoutProps = {
 const MainLayout = ({ children }: MainLayoutProps) => {
   return (
     <div className="min-h-scren w-full">
-      <main className="justify-cente m-auto max-w-xl flex-row">{children}</main>
+      <main className="m-auto max-w-xl">{children}</main>
     </div>
   );
 };
