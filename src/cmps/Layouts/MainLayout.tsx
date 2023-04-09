a81d06fb3198939b8f8f40ece046133a857b2432
Ryan Connaughton <hello@ryanconnaughton.com>
2023-04-09 14:37:41 +0100

    add: MainLayout boilerplate

---
+++ src/cmps/Layouts/MainLayout.tsx
@@ -0,0 +1,13 @@
+type MainLayoutProps = {
+  children: React.ReactNode;
+};
+
+const MainLayout = ({ children }: MainLayoutProps) => {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <main className="flex-grow">{children}</main>
+    </div>
+  );
+};
+
+export default MainLayout;
