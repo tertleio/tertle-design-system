d14fbcc358a35edd434cd53dec549d9616eec6ad
Ryan Connaughton <hello@ryanconnaughton.com>
2023-04-09 10:13:09 +0100

    Form: add focus styles

--- src/cmps/Form/FieldWrapper.tsx
+++ src/cmps/Form/FieldWrapper.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from 'react';
 
-const dark = `dark:text-gray-600 dark:border-[#141414] dark:bg-gray-800 dark:hover:bg-[#141414]`;
+const dark = `dark:text-gray-600 dark:border-[#141414] dark:bg-gray-800 dark:hover:bg-[#141414] dark:focus-within:border-b-gray-700`;
 
 export type FieldWrapperProps = {
   children: ReactNode;
@@ -12,7 +12,10 @@ export type FieldWrapperProps = {
   error?: string;
 };
 
-type FieldWrapperPassProps = Omit<FieldWrapperProps, 'children' | 'className'>;
+type FieldWrapperPassProps = Omit<
+  FieldWrapperProps,
+  'children' | 'className' | 'type'
+>;
 
 const FieldWrapper = (props: FieldWrapperProps) => {
   const { children, className, label, error, id, type } = props;
@@ -24,7 +27,7 @@ const FieldWrapper = (props: FieldWrapperProps) => {
         htmlFor={id}
         className={`
           my-[1px] flex cursor-pointer justify-start rounded-lg border border-gray-100 bg-transparent p-3
-         font-secondary text-gray-600 hover:bg-gray-100 hover:opacity-90
+         font-secondary text-gray-600 focus-within:border-b-gray-300 hover:bg-gray-100 hover:opacity-90
           ${dark}
           ${className}`}
       >
