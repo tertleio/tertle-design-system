fdb87e7765205203ac580565657d7702885c55a7
Ryan Connaughton <hello@ryanconnaughton.com>
2023-04-10 19:48:07 +0100

    MainLayout: rm min height style

--- src/cmps/Layouts/MainLayout.tsx
+++ src/cmps/Layouts/MainLayout.tsx
@@ -4,7 +4,7 @@ type MainLayoutProps = {
 
 const MainLayout = ({ children }: MainLayoutProps) => {
   return (
-    <div className="min-h-screen w-full">
+    <div className="w-full">
       <main className="m-auto max-w-xl">{children}</main>
     </div>
   );
