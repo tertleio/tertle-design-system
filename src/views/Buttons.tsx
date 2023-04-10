dca24072ac0ea24f0117e5bde6522126e503936d
Ryan Connaughton <hello@ryanconnaughton.com>
2023-04-10 19:21:03 +0100

    add: view folder and mv Buttons

---
+++ src/views/Buttons.tsx
@@ -0,0 +1,74 @@
+import { Button } from '@/cmps/Els/Button';
+
+const ButtonsPage = () => {
+  return (
+    <>
+      <Button size="sm" variant="primary" icon="github">
+        Primary Sm
+      </Button>
+      <Button size="md" variant="primary" icon="linkedin">
+        Primary md
+      </Button>
+      <Button size="lg" variant="primary" icon="twitter">
+        Primary lg
+      </Button>
+      <Button size="sm" variant="secondary">
+        Secondary Sm
+      </Button>
+      <Button size="sm" variant="secondary" icon="github">
+        Primary Sm
+      </Button>
+      <Button size="md" variant="secondary" icon="linkedin">
+        Primary md
+      </Button>
+      <Button size="lg" variant="secondary" icon="twitter">
+        Primary lg
+      </Button>
+      <Button size="sm" variant="tertiary" icon="github">
+        Primary Sm
+      </Button>
+      <Button size="md" variant="tertiary" icon="linkedin">
+        Primary md
+      </Button>
+      <Button size="lg" variant="tertiary" icon="twitter">
+        Primary lg
+      </Button>
+      <Button size="sm" variant="secondary">
+        Secondary Sm
+      </Button>
+      <Button size="md" variant="secondary">
+        Secondary Md
+      </Button>
+      <Button size="lg" variant="secondary">
+        Secondary Lg
+      </Button>
+      <Button size="sm" variant="tertiary">
+        Tertiary Sm
+      </Button>
+      <Button size="md" variant="tertiary">
+        Tertiary Md
+      </Button>
+      <Button size="lg" variant="tertiary">
+        Tertiary Lg
+      </Button>
+
+      <Button size="sm" variant="primary" icon="github" />
+      <Button size="md" variant="primary" icon="linkedin" />
+      <Button size="lg" variant="primary" icon="twitter" />
+      <Button size="sm" variant="secondary" icon="github" />
+      <Button size="md" variant="secondary" icon="linkedin" />
+      <Button size="lg" variant="secondary" icon="twitter" />
+      <Button size="sm" variant="tertiary" icon="github" />
+      <Button size="md" variant="tertiary" icon="linkedin" />
+      <Button size="lg" variant="tertiary" icon="twitter" />
+      <Button size="sm" variant="secondary" />
+      <Button size="md" variant="secondary" />
+      <Button size="lg" variant="secondary" />
+      <Button size="sm" variant="tertiary" />
+      <Button size="md" variant="tertiary" />
+      <Button size="lg" variant="tertiary" />
+    </>
+  );
+};
+
+export default ButtonsPage;
