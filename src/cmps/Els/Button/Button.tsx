41fdf9dfec6ea59b93413d6e7eff564114d20ad4
Ryan Connaughton <hello@ryanconnaughton.com>
2023-04-10 18:51:12 +0100

    Button: add tertiary and update styles with optional icon

--- src/cmps/Els/Button/Button.tsx
+++ src/cmps/Els/Button/Button.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
+import { Icon, IconMember } from '../Icon';
 
 const variants = {
   primary: 'bg-primary text-base dark:bg-primary-dark dark:text-base-dark',
   secondary: `border border-primary text-primary dark:text-primary-dark dark:border-primary-dark`,
+  tertiary: `text-gray-500 border border-gray-200 dark:border-gray-700 hover:bg-gray-200 dark:hover:bg-gray-700 text-white`,
   inverse: 'bg-white text-blue-600',
   danger: 'bg-red-600 text-white',
 };
 
 const sizes = {
-  sm: 'py-1.5 px-2 text-sm',
-  md: 'py-2 px-6 text-md',
-  lg: 'py-3 px-8 text-lg',
+  sm: 'py-1.5 px-[0.4rem] rounded-md text-sm font-secondary normal-case font-bold',
+  md: 'py-2 px-3 rounded-lg text-md',
+  lg: 'py-3 px-6 rounded-lg text-lg',
 };
 
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   variant?: keyof typeof variants;
+  type?: 'button' | 'submit' | 'reset';
+  icon?: IconMember;
   size?: keyof typeof sizes;
   isLoading?: boolean;
 };
@@ -23,27 +27,45 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   (
     {
       type = 'button',
-      className = '',
+      icon,
       variant = 'primary',
-      size = 'md',
+      size = 'sm',
       isLoading = false,
+      className = '',
       ...props
     },
     ref
   ) => {
+    const iconOnly = !props.children;
     return (
       <div>
         <button
           ref={ref}
           type={type}
           className={`
-            flex items-center justify-center rounded-lg font-primary font-medium uppercase
+            flex items-center justify-center font-primary font-medium uppercase
             hover:opacity-90 disabled:cursor-not-allowed disabled:opacity-70
+            ${iconOnly ? 'border-transparent dark:border-transparent' : ''}
             ${variants[variant]}
             ${sizes[size]}
             ${className}`}
           {...props}
-        ></button>
+        >
+          {icon && (
+            <Icon
+              name={icon}
+              size={size}
+              className={`
+                ${iconOnly ? 'mr-0' : 'mr-3'}
+                ${
+                  variant === 'primary'
+                    ? 'fill-white dark:fill-black'
+                    : 'fill-gray-500 dark:fill-gray-500'
+                }`}
+            />
+          )}
+          {props.children}
+        </button>
       </div>
     );
   }
