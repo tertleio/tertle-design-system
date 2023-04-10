0567ac2aace674df2d531676ad3355f404eeaa69
Ryan Connaughton <hello@ryanconnaughton.com>
2023-04-10 19:50:25 +0100

    App: updated test view with form

--- src/features/profile/routes/Profile.tsx
+++ src/features/profile/routes/Profile.tsx
@@ -54,71 +54,7 @@ const Profile = () => {
           title="Some other title that's abit longer"
           aside={<List data={dummyData.startupLinks} />}
         >
-          <h1>Some other section</h1>
-          <Button size="sm" variant="primary" icon="github">
-            Primary Sm
-          </Button>
-          <Button size="md" variant="primary" icon="linkedin">
-            Primary md
-          </Button>
-          <Button size="lg" variant="primary" icon="twitter">
-            Primary lg
-          </Button>
-          <Button size="sm" variant="secondary">
-            Secondary Sm
-          </Button>
-          <Button size="sm" variant="secondary" icon="github">
-            Primary Sm
-          </Button>
-          <Button size="md" variant="secondary" icon="linkedin">
-            Primary md
-          </Button>
-          <Button size="lg" variant="secondary" icon="twitter">
-            Primary lg
-          </Button>
-          <Button size="sm" variant="tertiary" icon="github">
-            Primary Sm
-          </Button>
-          <Button size="md" variant="tertiary" icon="linkedin">
-            Primary md
-          </Button>
-          <Button size="lg" variant="tertiary" icon="twitter">
-            Primary lg
-          </Button>
-          <Button size="sm" variant="secondary">
-            Secondary Sm
-          </Button>
-          <Button size="md" variant="secondary">
-            Secondary Md
-          </Button>
-          <Button size="lg" variant="secondary">
-            Secondary Lg
-          </Button>
-          <Button size="sm" variant="tertiary">
-            Tertiary Sm
-          </Button>
-          <Button size="md" variant="tertiary">
-            Tertiary Md
-          </Button>
-          <Button size="lg" variant="tertiary">
-            Tertiary Lg
-          </Button>
-
-          <Button size="sm" variant="primary" icon="github" />
-          <Button size="md" variant="primary" icon="linkedin" />
-          <Button size="lg" variant="primary" icon="twitter" />
-          <Button size="sm" variant="secondary" icon="github" />
-          <Button size="md" variant="secondary" icon="linkedin" />
-          <Button size="lg" variant="secondary" icon="twitter" />
-          <Button size="sm" variant="tertiary" icon="github" />
-          <Button size="md" variant="tertiary" icon="linkedin" />
-          <Button size="lg" variant="tertiary" icon="twitter" />
-          <Button size="sm" variant="secondary" />
-          <Button size="md" variant="secondary" />
-          <Button size="lg" variant="secondary" />
-          <Button size="sm" variant="tertiary" />
-          <Button size="md" variant="tertiary" />
-          <Button size="lg" variant="tertiary" />
+          <h1>Some stuff about me</h1>
         </Section>
       </Container>
     </MainLayout>
