6e7bf6255ba97013c6c9abba3f2c47ff9bc0e42a
Ryan Connaughton <hello@ryanconnaughton.com>
2023-04-11 10:24:47 +0100

    Section: update body text color

--- src/cmps/Els/Section/Section.tsx
+++ src/cmps/Els/Section/Section.tsx
@@ -1,6 +1,6 @@
 import { Icon, IconMember } from '@/cmps/Els/Icon';
 
-const twHeader = `text-1xl lg:text-1xl font-primary text-black dark:text-white md:text-xl`;
+const twHeader = `text-1xl lg:text-1xl font-primary text-black dark:text-white sm:text-xl`;
 
 type SectionProps = {
   title?: string;
@@ -15,12 +15,12 @@ const Section = (props: SectionProps) => {
   return (
     <div
       className={`
-        border-b border-gray-300 p-4 first:rounded-t-3xl first:py-9 last:rounded-b-3xl last:border-b-0 last:pb-8 hover:cursor-pointer hover:bg-gray-100
-        dark:border-gray-700 dark:hover:bg-[#1D1D1D] md:p-8
+        border-b border-gray-300 p-4 text-gray-600 first:rounded-t-3xl first:py-6 last:rounded-b-3xl last:border-b-0 last:pb-8 hover:cursor-pointer hover:bg-gray-100
+        dark:border-gray-700 dark:hover:bg-[#1D1D1D] sm:p-8 sm:first:py-9
         ${className}`}
     >
       <div className="flex justify-between">
-        <h2 className="lg:text-1xl font-primary text-[1.2em] text-black dark:text-white md:text-2xl">
+        <h2 className="lg:text-1xl font-primary text-[1.2em] text-black dark:text-white sm:text-2xl">
           {title}
         </h2>
         <div>
