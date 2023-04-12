4e68d8d3ac63537e1fb161b5c0053d2b3e0c257e
Ryan Connaughton <hello@ryanconnaughton.com>
2023-04-12 18:28:46 +0100

    IconMember: type update

--- src/cmps/Els/Icon/Icon.tsx
+++ src/cmps/Els/Icon/Icon.tsx
@@ -1,17 +1,17 @@
 // imported icon base size must be 16x16px
 import * as icons from '@/assets/icons';
 
-type IconMember = keyof typeof icons;
-type IconProps = {
-  name: IconMember;
-  size?: 'sm' | 'md' | 'lg';
-  className?: string;
-};
-
 const sizes = {
   sm: 'w-[14px] h-[14px]',
   md: 'w-[18px] h-[18px]',
   lg: 'w-[20px] h-[20px]',
+};
+
+type IconMember = keyof typeof icons;
+type IconProps = {
+  name: IconMember;
+  size?: keyof typeof sizes;
+  className?: string;
 };
 
 const Icon = (props: IconProps) => {
