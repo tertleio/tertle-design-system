504f7d52f9d34f93f4f9b424d3e51e122969cdd6
Ryan Connaughton <hello@ryanconnaughton.com>
2023-04-12 15:15:23 +0100

    tw.config: add some colors, to be added to tokesn repo later

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,25 @@ module.exports = {
   content: ['./src/**/*.{html,js,ts,tsx}'],
   darkMode: 'class', // false | 'class' | 'media'
   theme: {
-    colors: { ...tokens.colors, transparent: 'transparent' },
+    colors: {
+      ...tokens.colors,
+      green: {
+        DEFAULT: tokens.colors.primary.DEFAULT,
+        light: tokens.colors.primary.light,
+        dark: tokens.colors.primary.light,
+      },
+      orange: {
+        DEFAULT: '#C97C00',
+        light: '#C97C00',
+        dark: '#FF5C28',
+      },
+      red: {
+        DEFAULT: '#BC2929',
+        light: '#BC2929',
+        dark: '#EC3D3D',
+      },
+      transparent: 'transparent',
+    },
     extend: {
       fontFamily: {
         primary: ['poppins', 'Helvetica', 'Arial', 'sans-serif'],
