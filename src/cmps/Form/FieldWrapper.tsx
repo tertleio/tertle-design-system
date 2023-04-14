712b527f4dd335bec04d62840beee1f03cc0413c
Ryan Connaughton <hello@ryanconnaughton.com>
2023-04-14 13:04:49 +0100

    FieldWrapper: refactor ugly template literals everywhere with clsx

--- src/cmps/Form/FieldWrapper.tsx
+++ src/cmps/Form/FieldWrapper.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react';
+import clsx from 'clsx';
 
 const twText = `dark:border-b-gray-700 border-b-gray-300 dark:border-transparent border-transparent hover:bg-transparent dark:hover:bg-transparent focus-within:border-b-primary dark:focus-within:border-b-primary-dark`;
 
@@ -27,15 +28,14 @@ const FieldWrapper = (props: FieldWrapperProps) => {
       <label
         htmlFor={id}
         // prettier-ignore
-        className={`        
-            dark:hover:bg-700 flex cursor-pointer items-center justify-start rounded-lg 
-            border font-secondary text-gray-600 p-2 px-2.5
-           hover:bg-gray-100  dark:hover:bg-gray-800 border-gray-100 dark:border-gray-800
-           ${isText && twText}
-           ${readOnly && 'pointer-events-none border-transparent dark:border-transparent'}
-           ${readOnly && isText && 'border-b-transparent dark:border-b-transparent'}
-           ${className}
-        `}
+        className={clsx(
+          `dark:hover:bg-700 flex cursor-pointer items-center justify-start rounded-lg border 
+           font-secondary text-gray-600 p-2 px-2.5 hover:bg-gray-100 dark:hover:bg-gray-800 border-gray-100 dark:border-gray-800`,
+           isText && twText,
+           readOnly && 'pointer-events-none border-transparent dark:border-transparent',
+           readOnly && isText && 'border-b-transparent dark:border-b-transparent',
+           className
+        )}
       >
         {children}
         {label}
