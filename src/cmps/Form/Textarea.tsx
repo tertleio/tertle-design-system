21fccb301208b16def97027ec4d4a9e260fa4481
Ryan Connaughton <hello@ryanconnaughton.com>
2023-04-15 09:03:08 +0100

    Textarea: add w-full back'

--- src/cmps/Form/Textarea.tsx
+++ src/cmps/Form/Textarea.tsx
@@ -49,7 +49,7 @@ const Textarea = (props: TextareaProps) => {
         value={value}
         onChange={onChange}
         className={clsx(
-          'bg-transparent placeholder:text-gray-400 focus:outline-none dark:placeholder:text-gray-700',
+          'w-full bg-transparent  placeholder:text-gray-400 focus:outline-none dark:placeholder:text-gray-700',
           'resize-none',
           className
         )}
