d182fec3a6b037d5609dc6eb7f9b2271b754314c
Ryan Connaughton <hello@ryanconnaughton.com>
2023-04-16 11:27:36 +0100

    Icon: const import

--- src/cmps/Els/Icon/Icon.tsx
+++ src/cmps/Els/Icon/Icon.tsx
@@ -1,11 +1,11 @@
 // imported icon base size must be 16x16px
-import * as Icons from '@/assets/icons';
+import * as ICONS from '@/assets/icons';
 import clsx from 'clsx';
 
 // prettier-ignore
 const icons = {
   spinner: ['M4 12a8 8 1 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'],
-  ...Icons
+  ...ICONS
 }
 
 const sizes = {
